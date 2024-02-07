9d34a22d8965c11582536830c04b61b73bffac9a
stang1543 <you@example.com>
2024-02-07 12:23:11 +0700

    first commit

---
+++ flutter-quiz-app-tutorial-main/lib/models/questions.dart
@@ -0,0 +1,106 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. เกมฟีฟ่าภาคแรกวางจำหน่ายในปีใด?',
+    correctAnswerIndex: 0,
+    options: [
+      'ก. 1993 ',
+      'ข. 1994',
+      'ค. 1995',
+      'ง. 1996',
+    ],
+  ),
+  Question(
+    question: '2. ใครเป็นผู้พัฒนาเกมฟีฟ่า?',
+    correctAnswerIndex: 0,
+    options: [
+      'ก. Electronic Arts (EA)',
+      'ข. Konami',
+      'ค. Ubisoft',
+      'ง. Sega',
+    ],
+  ),
+  Question(
+    question: '3. เกมฟีฟ่าภาคใดเป็นภาคแรกที่มีระบบออนไลน์?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. ฟีฟ่า 97',
+      'ข. ฟีฟ่า 98',
+      'ค. ฟีฟ่า 99',
+      'ง. ฟีฟ่า 2000',
+    ],
+  ),
+  Question(
+    question: '4. ทีมชาติใดเป็นทีมที่คะแนนสูงสุดในเกมฟีฟ่า 23?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. บราซิล',
+      'ข. อาร์เจนตินา',
+      'ค. ฝรั่งเศส',
+      'ง. เบลเยียม',
+    ],
+  ),
+  Question(
+    question: '5. โหมดไหนในเกมฟีฟ่าที่ผู้เล่นสามารถสร้างนักเตะของตัวเองได้?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. Career Mode',
+      'ข. Ultimate Team',
+      'ค. Pro Clubs',
+      'ง. Volta Football',
+    ],
+  ),
+  Question(
+    question: '6. เกมฟีฟ่าภาคใดเป็นภาคแรกที่มีระบบ Ultimate Team?',
+    correctAnswerIndex: 0,
+    options: [
+      'ก. ฟีฟ่า 09',
+      'ข. ฟีฟ่า 10',
+      'ค. ฟีฟ่า 11',
+      'ง. ฟีฟ่า 12',
+    ],
+  ),
+  Question(
+    question: '7. ใครเป็นนักเตะที่ขึ้นปกเกมฟีฟ่ามากที่สุด?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. โรนัลโด้ (บราซิล)',
+      'ข. คริสเตียโน่ โรนัลโด้ (โปรตุเกส)',
+      'ค. ลิโอเนล เมสซี่ (อาร์เจนตินา)',
+      'ง. ทั้งหมดเท่ากัน',
+    ],
+  ),
+  Question(
+    question: '8. เกมฟีฟ่าภาคใดเป็นภาคแรกที่วางจำหน่ายบน PlayStation 4 และ Xbox One??',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. ฟีฟ่า 14',
+      'ข. ฟีฟ่า 15',
+      'ค. ฟีฟ่า 16',
+      'ง. ฟีฟ่า 17',
+    ],
+  ),
+  Question(
+    question:
+        ' 9. เกมฟีฟ่าภาคใดเป็นภาคแรกที่มีระบบ Volta Football??',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. ฟีฟ่า 18',
+      'ข. ฟีฟ่า 19',
+      'ค. ฟีฟ่า 20',
+      'ง. ฟีฟ่า 21',
+    ],
+  ),
+  Question(
+    question:
+        ' 10. เกมฟีฟ่าภาคใดเป็นภาคล่าสุด?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. ฟีฟ่า 23',
+      'ข. ฟีฟ่า 24',
+      'ค. ฟีฟ่า 25',
+      'ง. ฟีฟ่า 26',
+    ],
+  ),
+];
